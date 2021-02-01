41fa5aa25344c2d3d6a7ae70185e52e577e45dfa
Azzouz <menaaziz27@gmail.com>
2021-02-01 16:24:40 +0200

    live search

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list");
+
+const searchStates = async (searchText) => {
+	let res = await fetch("../data/data.json");
+	let states = await res.json();
+
+	let matches = states.filter((state) => {
+		const regex = new RegExp(`^${searchText}`, "gi");
+		return state.name.match(regex) || state.abbr.match(regex);
+	});
+
+	if (searchText.length === 0) {
+		matches = [];
+		matchList.innerHTML = "";
+	}
+
+	outputHtml(matches);
+};
+
+// show results in html
+const outputHtml = (matches) => {
+	if (matches.length > 0) {
+		const html = matches.map(
+			(match) =>
+				`
+        <div class="card card-body mb-1">
+          <h4>${match.name} (${match.abbr})<span class="text-primary">${match.capital}</span></h4>
+          <small>Lat: ${match.lat} / Long: ${match.long}</small>
+        </div>
+      `
+		);
+		matchList.innerHTML = html;
+	}
+};
+
+search.addEventListener("input", () => searchStates(search.value));
